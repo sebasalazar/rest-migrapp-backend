3e3517046904fa01c59b9a7c8047cdb33ff9837e
Sebastián Salazar Molina <sebasalazar@gmail.com>
2020-11-02 21:56:17 -0300

    Cambios por v1

--- app/src/main/java/cl/sebastian/proyecto/rest/api/v1/CountryRest.java
+++ app/src/main/java/cl/sebastian/proyecto/rest/api/v1/CountryRest.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = {"/countries"}, consumes = {"application/json; charset=UTF-8"}, produces = {"application/json; charset=UTF-8"})
+@RequestMapping(value = {"/v1/countries"}, consumes = {"application/json; charset=UTF-8"}, produces = {"application/json; charset=UTF-8"})
 @Api(value = "Información relativa a los países")
 public class CountryRest implements Serializable {
 
--- app/src/main/java/cl/sebastian/proyecto/rest/api/v1/IndicatorRest.java
+++ app/src/main/java/cl/sebastian/proyecto/rest/api/v1/IndicatorRest.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = {"/indicators"}, consumes = {"application/json; charset=UTF-8"}, produces = {"application/json; charset=UTF-8"})
+@RequestMapping(value = {"/v1/indicators"}, consumes = {"application/json; charset=UTF-8"}, produces = {"application/json; charset=UTF-8"})
 @Api(value = "Información relativa a los indicadores económicos")
 public class IndicatorRest implements Serializable {
 
